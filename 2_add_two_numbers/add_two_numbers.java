6ba8f6e5ae7bd7322934b67af510cb2d40b56def
Jimmy <Jimmy@Jimmy-Mac.local>
2016-05-18 13:44:02 -0400

    Solve Problem 2 : Add Two Numbers

---
+++ 2_add_two_numbers/add_two_numbers.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int temp = carry;
+            if(l1 != null){
+                temp += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                temp += l2.val;
+                l2 = l2.next;
+            }
+            p.next = new ListNode(temp % 10);
+            carry = temp / 10;
+            p = p.next;
+        }
+        if(carry != 0){
+            p.next = new ListNode(carry);
+        }
+        return dummy.next;
+        
+    }
+}
