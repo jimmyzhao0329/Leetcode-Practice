71004e3bf9953028705911a7d682033b91ab67cc
Jimmy <Jimmy@Jimmy-Mac.local>
2016-05-18 10:16:17 -0400

    Upload 1_two_sum

---
+++ 1_two_sum/two_sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class two_sum{
+	public int[] twoSum(int[] nums, int target){
+		int[] res = {0, 0};
+		Map<Integer, Integer> mp = new HashMap<>();
+		for(int i = 0; i < nums.length; i++){
+			if(mp.containsKey(target - nums[i])){
+				res[0] = mp.get(target - nums[i]);
+				res[1] = i;
+				break;
+			}
+			else{
+				mp.put(nums[i], i);
+			}
+		}
+		return res;
+	}
+}
