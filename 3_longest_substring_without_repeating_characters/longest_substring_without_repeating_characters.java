5f3671d242df76a5a8a4af4b099d943a8be33c0d
Jimmy <Jimmy@Jimmy-Mac.local>
2016-05-18 16:15:42 -0400

    Solve Longest Substring Without Repeating Characters

---
+++ 3_longest_substring_without_repeating_characters/longest_substring_without_repeating_characters.java
@@ -0,0 +1,23 @@
+//instead of using hash map, use index[128] will save even more time
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() <= 1){
+            return s.length();
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int startptr = 0, count = 0, maxCount = 1;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                startptr = Math.max(map.get(s.charAt(i)) + 1, startptr);
+            }
+            count = i - startptr + 1;
+            maxCount = Math.max(maxCount, count);
+            map.put(s.charAt(i), i);
+        }
+        
+        return maxCount;
+    }
+}
