d0e0e0afc36b0a8f30cf4f26afa95e97b5aa516e
Jimmy <Jimmy@Jimmy-Mac.local>
2016-05-19 23:18:00 -0400

    Solve Container With Most Water

---
+++ 11_Container_With_Most_Water/Container_With_Most_Water.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int vol = 0, maxVol = 0;
+        int l = 0, r = height.length - 1;
+        while(l < r){
+            vol = Math.min(height[l], height[r]) * (r - l);
+            maxVol = Math.max(maxVol, vol);
+            if(height[l] < height[r]){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return maxVol;
+    }
+}
