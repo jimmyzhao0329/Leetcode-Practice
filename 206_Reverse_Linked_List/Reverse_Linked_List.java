5b9becfcf3218e397c7906ddf0f71ae2c1db5240
Jimmy <Jimmy@Jimmy-Mac.local>
2016-06-01 21:48:01 -0400

    Solve Reverse_Linked_List

---
+++ 206_Reverse_Linked_List/Reverse_Linked_List.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+}
