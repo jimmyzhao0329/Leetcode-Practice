53c4ad2f53cc302c5016568f1b4a609792e8fc3c
Jimmy <Jimmy@Jimmy-Mac.local>
2016-06-03 08:09:05 -0400

    Solve Unique_Paths_II

---
+++ 063_Unique_Paths_II/Unique_Paths_II.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/unique-paths-ii/
+
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] path = new int[m][n];
+        
+        for(int i = 0; i < n; i++){
+            if(grid[0][i] != 1){
+                path[0][i] = 1;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(int i = 0; i < m; i++){
+            if(grid[i][0] != 1){
+                path[i][0] = 1;
+            }
+            else{
+                break;
+            }
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] != 1){
+                    path[i][j] = path[i - 1][j] + path[i][j - 1];
+                }
+            }
+        }
+        
+        return path[m - 1][n - 1];
+    }
+}
