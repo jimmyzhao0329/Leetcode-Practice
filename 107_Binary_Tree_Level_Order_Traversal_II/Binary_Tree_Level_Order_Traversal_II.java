bee2eb653ce02933f0a0bd1f89f101cce3595873
Jimmy <Jimmy@Jimmy-Mac.local>
2016-06-15 14:31:37 -0400

    Solve Binary_Tree_Level_Order_Traversal_II

--- 107_Binary_Tree_Level_Order_Traversal_II/Binary_Tree_Level_Order_Traversal_II.java
+++ 107_Binary_Tree_Level_Order_Traversal_II/Binary_Tree_Level_Order_Traversal_II.java
@@ -10,35 +10,28 @@
  * }
  */
 public class Solution {
-    private void helper(List<List<Integer>> res, TreeNode root){
-        if(root == null){
-            return;
-        }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        while(!q.isEmpty()){
-            List<Integer> cur = new ArrayList<Integer>();
-            int len = q.size();
-            for(int i = 0; i < len; i++){
-                TreeNode node = q.remove();
-                if(node.left != null){
-                    q.add(node.left);
-                }
-                if(node.right != null){
-                    q.add(node.right);
-                }
-                cur.add(node.val);
-            }
-            res.add(0, new ArrayList<Integer>(cur));
-        }
-    }
-    
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(root == null){
             return res;
         }
-        helper(res, root);
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> cur = new ArrayList<Integer>();
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+                TreeNode node = q.poll();
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+                cur.add(node.val);
+            }
+            res.add(0, cur);
+        }
         return res;
     }
 }
