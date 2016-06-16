7276bd199b1ac0d4dec5088bb550443e375012a4
Jimmy <Jimmy@Jimmy-Mac.local>
2016-06-16 10:22:42 -0400

    Solve Minimum_Depth_of_Binary_Tree

---
+++ 111_Minimum_Depth_of_Binary_Tree/11_Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left != null && root.right != null){
+            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+        else{
+            return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+        }
+    }
+}
