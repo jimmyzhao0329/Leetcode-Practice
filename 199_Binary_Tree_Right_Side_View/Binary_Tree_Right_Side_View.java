22f5c7daa6ff255eb4f4f4519c073eba27a01ecd
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-07 10:00:03 -0400

    Solve Binary_Tree_Right_Side_View

---
+++ 199_Binary_Tree_Right_Side_View/Binary_Tree_Right_Side_View.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+                TreeNode node = q.poll();
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+                if(i == len - 1){
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
