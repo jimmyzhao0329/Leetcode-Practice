0dba31e3ff75808be53dd771b6ef4854128879d5
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-11 10:46:25 -0400

    Solve Add_and_Search_Word_Data_structure_design

---
+++ 211_Add_and_Search_Word_Data_structure_design/Add_and_Search_Word_Data_structure_design.java
@@ -0,0 +1,71 @@
+// https://leetcode.com/problems/add-and-search-word-data-structure-design/
+
+public class WordDictionary {
+    
+    public class TrieNode {
+        public char val;
+        public boolean isWord;
+        public TrieNode[] next = new TrieNode[26];
+        public TrieNode() {}
+        public TrieNode(char c){
+            TrieNode node = new TrieNode();
+            node.val = c;
+        }
+    }
+    
+    private TrieNode root;
+    
+    public WordDictionary(){
+        root = new TrieNode(' ');
+    }
+    
+    // Adds a word into the data structure.
+    public void addWord(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(node.next[c - 'a'] == null){
+                node.next[c - 'a'] = new TrieNode(c);
+            }
+            node = node.next[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return helper(word, 0, root);
+    }
+    
+    private boolean helper(String word, int index, TrieNode parent){
+        TrieNode node = parent;
+        for(int i = index; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(c != '.'){
+                if(node.next[c - 'a'] == null){
+                    return false;
+                }
+                else{
+                    node = node.next[c - 'a'];
+                }
+            }
+            else{
+                for(char l = 'a'; l <= 'z'; l++){
+                    if(node.next[l - 'a'] != null){
+                        if(helper(word, i + 1, node.next[l - 'a'])){
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        return (node != null && node.isWord);
+    }
+}
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
