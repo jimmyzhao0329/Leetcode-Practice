3143ef819693e257fec9fac1bc91a58702417ca3
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-12 10:43:04 -0400

    Solve Contains_Duplicate_III.java

---
+++ 219_Contains_Duplicate_II/220_Contains_Duplicate_III/Contains_Duplicate_III.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/contains-duplicate-iii/
+
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+
+        TreeSet<Integer> values = new TreeSet<>();
+        for (int ind = 0; ind < nums.length; ind++) {
+
+            Integer floor = values.floor(nums[ind] + t);
+            Integer ceil = values.ceiling(nums[ind] - t);
+            if ((floor != null && floor >= nums[ind])
+                    || (ceil != null && ceil <= nums[ind])) {
+                return true;
+            }
+
+            values.add(nums[ind]);
+            if (ind >= k) {
+                values.remove(nums[ind - k]);
+            }
+        }
+
+        return false;
+    }
+}
