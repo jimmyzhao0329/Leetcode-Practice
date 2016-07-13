fca9ef3286552aaa4c93192ff948ba71b8aec596
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-13 08:34:04 -0400

    Solve Implement_Stack_using_Queues

---
+++ 225_Implement_Stack_using_Queues/Implement_Stack_using_Queues.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack {
+    
+    private Queue<Integer> q = new LinkedList<Integer>();
+    
+    // Push element x onto stack.
+    public void push(int x) {
+        q.offer(x);
+        for(int i = 0; i < q.size() - 1; i++){
+            q.offer(q.poll());
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        q.poll();
+    }
+
+    // Get the top element.
+    public int top() {
+        return q.peek();
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
