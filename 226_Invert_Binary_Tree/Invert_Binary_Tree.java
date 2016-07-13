d8d21fe6b41bc7d06a3a1561a7c58ff0d8976c1d
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-13 08:52:40 -0400

    Solve Invert_Binary_Tree

--- 226_Invert_Binary_Tree/Invert_Binary_Tree.java
+++ 226_Invert_Binary_Tree/Invert_Binary_Tree.java
@@ -11,6 +11,8 @@
  */
 public class Solution {
     public TreeNode invertTree(TreeNode root) {
+        
+        /*  recursion
         if(root == null){
             return null;
         }
@@ -18,6 +20,27 @@ public class Solution {
         TreeNode right = root.right;
         root.left = invertTree(right);
         root.right = invertTree(left);
+        return root;
+        */
+        
+        if(root == null){
+            return null;
+        }
+        
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            if(node.left != null){
+                q.offer(node.left);
+            }
+            if(node.right != null){
+                q.offer(node.right);
+            }
+        }
         return root;
     }
 }
