591f18f39e2e6fa0822388c9b7ab3a05ada2f4aa
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-18 10:14:11 -0400

    Update Permutations.java

--- 046_Permutations/Permutations.java
+++ 046_Permutations/Permutations.java
@@ -2,28 +2,27 @@
 
 public class Solution {
     
-    private void helper(List<List<Integer>> res, List<Integer> cur, int[] nums, int start){
-        for(int i = 0; i < nums.length; i++){
-            cur.add(nums[i]);
+    private void helper(List<List<Integer>> res, List<Integer> cur, int[] nums){
+        if(cur.size() == nums.length){
+            res.add(new ArrayList<Integer>(cur));
+            return;
         }
-        res.add(new ArrayList<Integer>(cur));
-        
-        for(int i = start; i < nums.length - 1; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                helper(res, new ArrayList<Integer>(), nums, i + 1);
-                temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        for(int i = 0; i < nums.length; i++){
+            if(cur.contains(nums[i])){
+                continue;
             }
+            cur.add(nums[i]);
+            helper(res, cur, nums);
+            cur.remove(cur.size() - 1);
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        helper(res, new ArrayList<Integer>(), nums, 0);
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        helper(res, new ArrayList<Integer>(), nums);
         return res;
     }
 }
