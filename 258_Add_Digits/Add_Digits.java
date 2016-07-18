936455b9e4458a9b6f790d7f172e116c71bf8e89
Jimmy <Jimmy@Jimmy-Mac.local>
2016-07-18 10:41:33 -0400

    Solve Add_Digits

---
+++ 258_Add_Digits/Add_Digits.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/add-digits/
+
+public class Solution {
+    public int addDigits(int num) {
+        if(num < 10){
+            return num;
+        }
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        return addDigits(sum);
+    }
+}
