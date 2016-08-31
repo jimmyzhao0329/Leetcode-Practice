60287d5f7b0cdcba4287cab853c829a8addcd0d8
Jimmy <Jimmy@Jimmy-Mac.local>
2016-08-31 17:18:21 -0400

    Solve Serialize_and_Deserialize_Binary_Tree

--- 297_Serialize_and_Deserialize_Binary_Tree/Serialize_and_Deserialize_Binary_Tree.java
+++ 297_Serialize_and_Deserialize_Binary_Tree/Serialize_and_Deserialize_Binary_Tree.java
@@ -1,5 +1,62 @@
 // https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
 
+// My solution
+public class Codec {
+
+    // Encodes a tree to a single string.
+    // Using level-order traversal 
+    public String serialize(TreeNode root) {
+        if(root == null){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            if(sb.length() != 0){
+                sb.append(",");
+            }
+            if(node != null){
+                sb.append(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
+            }
+            else{
+                sb.append("null");
+            }
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data == null || data.length() == 0){
+            return null;
+        }
+        String[] vals = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        int i = 1;
+        while(!q.isEmpty()){
+            TreeNode node = q.poll();
+            String s = vals[i++];
+            if(!s.equals("null")){
+                node.left = new TreeNode(Integer.parseInt(s));
+                q.offer(node.left);
+            }
+            s = vals[i++];
+            if(!s.equals("null")){
+                node.right = new TreeNode(Integer.parseInt(s));
+                q.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
+
+// Highest voted solution
 public class Codec {
     private static final String spliter = ",";
     private static final String NN = "X";
