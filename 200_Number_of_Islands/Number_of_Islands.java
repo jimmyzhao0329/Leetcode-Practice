d422dda743adfe6797f331b2aea56cdacb2bb891
Jimmy <Jimmy@Jimmy-Mac.local>
2016-09-04 13:47:45 -0400

    Solve Number_of_Islands

--- 200_Number_of_Islands/Number_of_Islands.java
+++ 200_Number_of_Islands/Number_of_Islands.java
@@ -1,36 +1,30 @@
-// public class Solution {
+// https://leetcode.com/problems/number-of-islands/
+
+public class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        int row = grid.length;
-        if(row == 0){
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
             return 0;
         }
-        int col = grid[0].length;
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        int count = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    helper(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
         return count;
     }
     
-    private void helper(char[][] grid, int i, int j){
+    private void dfs(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1'){
+            return;
+        }
         grid[i][j] = '*';
-        if(i > 0 && grid[i - 1][j] == '1'){
-            helper(grid, i - 1, j);
-        }
-        if(i < grid.length - 1 && grid[i + 1][j] == '1'){
-            helper(grid, i + 1, j);
-        }
-        if(j > 0 && grid[i][j - 1] == '1'){
-            helper(grid, i, j - 1);
-        }
-        if(j < grid[0].length - 1 && grid[i][j + 1] == '1'){
-            helper(grid, i, j + 1);
-        }
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+        dfs(grid, i + 1, j);
     }
 }
-
