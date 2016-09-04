ed73601fa9fa56890d61d7e13f3fbab1e7825a59
Jimmy <Jimmy@Jimmy-Mac.local>
2016-09-04 13:08:52 -0400

    Solve Rotate_Image

--- 048_Rotate_Image/Rotate_Image.java
+++ 048_Rotate_Image/Rotate_Image.java
@@ -1,28 +1,23 @@
 //https://leetcode.com/problems/rotate-image/
 
 public class Solution {
-    public void rotate(int[][] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n - 1; i++){
-            for(int j = i + 1; j < n; j++){
-                int temp = nums[i][j];
-                nums[i][j] = nums[j][i];
-                nums[j][i] = temp;
+    public void rotate(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return;
+        }
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for(int i = 0; i < n; i++){
-            reverseArray(nums[i], 0, n - 1);
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - j - 1];
+                matrix[i][matrix[0].length - j - 1] = temp;
+            }
         }
-    }
-    
-    private static void reverseArray(int arr[], int start, int end)
-    {
-        int temp;
-        if (start >= end)
-            return;
-        temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        reverseArray(arr, start+1, end-1);
     }
 }
