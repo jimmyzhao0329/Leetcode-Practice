f2ce0582762fcf06430d1f538c06ecd91bddd041
Jimmy <Jimmy@Jimmy-Mac.local>
2016-09-17 16:21:26 -0400

    Solve Sliding_Window_Maximum

---
+++ 239_Sliding_Window_Maximum/Sliding_Window_Maximum.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/sliding-window-maximum/
+
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || nums.length == 0 || k <= 0){
+            return new int[0];
+        }
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        int resIndex = 0;
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+        for(int i = 0; i < len; i++){
+            // remove item that is out of window boundary
+            while(!dq.isEmpty() && dq.peek() < i - k + 1){
+                dq.poll();
+            }
+            // remove item that is smaller than a[i] from tail
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+            dq.offer(i);
+            if(i >= k - 1){
+                res[resIndex++] = nums[dq.peek()];
+            }
+        }
+        return res;
+    }
+}
