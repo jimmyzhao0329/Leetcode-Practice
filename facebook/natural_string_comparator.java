bf296274255f5490d5a516867697237f357c0b12
Jimmy <Jimmy@vagvlan531.1010.apn.wlan.wireless-pennnet.upenn.edu>
2016-11-03 16:27:41 -0400

    Update

--- facebook/natural_string_comparator.java
+++ facebook/natural_string_comparator.java
@@ -7,19 +7,20 @@ public class natural_string_comparator{
 	   int i;
 	   char c1, c2;
 	   for (i = 0, c1 = 0, c2 = 0; 
-	   		(i < len1) && (i < len2) && (c1 = s1.charAt(i)) == (c2 = s2.charAt(i)); 
+	   		(i < len1) && (i < len2) && 
+	   		(c1 = s1.charAt(i)) == (c2 = s2.charAt(i)); 
 	   		i++);
 
 	   // Check end of string
 	   if (c1 == c2)
-	      return(len1 - len2);
+	      return (len1 - len2);
 
 	   // Check digit in first string
 	   if (Character.isDigit(c1))
 	   {
 	      // Check digit only in first string
 	      if (!Character.isDigit(c2))
-	         return((i > 0) && Character.isDigit(s1.charAt(i - 1)) ? 1 : c1 - c2);
+	         return ((i > 0) && Character.isDigit(s1.charAt(i - 1)) ? 1 : c1 - c2);
 
 	      // Scan all integer digits
 	      int x1, x2;
@@ -27,12 +28,12 @@ public class natural_string_comparator{
 	      for (x2 = i + 1; (x2 < len2) && Character.isDigit(s2.charAt(x2)); x2++);
 
 	      // Longer integer wins, first digit otherwise
-	      return(x2 == x1 ? c1 - c2 : x1 - x2);
+	      return (x2 == x1 ? c1 - c2 : x1 - x2);
 	   }
 
 	   // Check digit only in second string
 	   if (Character.isDigit(c2))
-	      return((i > 0) && Character.isDigit(s2.charAt(i - 1)) ? -1 : c1 - c2);
+	      return ((i > 0) && Character.isDigit(s2.charAt(i - 1)) ? -1 : c1 - c2);
 
 	   // No digits
 	   return(c1 - c2);
